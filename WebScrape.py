37a6df05c5c1d256a8be661b70be4f2c394ac1aa
Ethan Jobe <ejobe98@gmail.com>
2023-01-18 20:22:29 -0600

    Added pymongo code. Checks if in DB.

--- WebScrape.py
+++ WebScrape.py
@@ -1,12 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
-url = 'https://www.indeed.com/jobs?q=software+developer&l=New+York'
+# Connect to the MongoDB database
+client = MongoClient()
+db = client.job_db
+jobs_collection = db.jobs 
+# Store job titles in jobs_collection
 
+# Establish connection, then parse the text
+url = 'https://www.indeed.com/jobs?q=mechanic&l=Texas'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 job_titles = soup.find_all('a', {'class': 'jobtitle'})
 
 for title in job_titles:
-    print(title.text)
+    job_title = title.text.strip()
+
+    # Check if the job already exists in the database
+    if not jobs_collection.find_one({'title': job_title}):
+        # insert the job into the database
+        jobs_collection.insert_one({'title': job_title})
+        print(f'{job_title} added to the database')
+    else:
+        print(f'{job_title} already exists in the database')
