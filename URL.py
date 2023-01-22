4351e599169ece6838c20d3cbebffd11d6deb6eb
Ethan Jobe <ejobe98@gmail.com>
2023-01-22 01:37:06 -0600

    URL comment, WebScrape unfinished

--- URL.py
+++ URL.py
@@ -4,6 +4,9 @@ import requests
 class TestUrlConnection(unittest.TestCase):
     def test_url_connection(self):
         url = 'https://chatgpt-website-nick-white.vercel.app/'
+        # When given a login:
+        # payload = {'username': 'myusername', 'password': 'mypassword'}
+        # response = requests.post(login_url, data=payload)
         response = requests.get(url)
         self.assertEqual(response.status_code, 200)
         print(response.content)
--- WebScrape.py
+++ WebScrape.py
@@ -1,7 +1,18 @@
 import requests
-import unittest
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+
+def DatabaseTest():
+    # Counting the number of documents in the collection
+    number_of_jobs = jobs_collection.count_documents({})
+    print(f'Number of jobs in the collection: {number_of_jobs}')
+
+    #Checking if there are any documents in the collection
+    if number_of_jobs > 0:
+        print("Jobs were successfully inserted into the database")
+    else:
+        print("No jobs were inserted into the database")
+
 
 # Connect to the MongoDB database
 client = MongoClient()
@@ -13,15 +24,31 @@ url = 'https://chatgpt-website-nick-white.vercel.app/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-job_titles = soup.find_all('h3', {'class': 'job-title'})
+job_titles = soup.find_all("div", {"class": "job"})
 
 for title in job_titles:
-    job_title = title.text.strip()
-    print(job_title)
-    # Check if the job already exists in the database
-    if not jobs_collection.find_one({'title': job_title}):
-        # Insert the job into the database
-        jobs_collection.insert_one({'title': job_title})
-        print(f'{job_title} added to the database')
+    position = title.find("h2", {"class": "job-title"}) # Note:job-title is from the html on *this* site; prepare to change it.
+    location = title.find("div", {"class": "job-location"})
+
+    #Check for job and location
+    if position and location and "Full Stack Developer" in position.text and "NY" in location.text:
+        job_title = position.text.strip()
+        job_location = location.text.strip()
+        print(f'{job_title} in {job_location}')
+
+        # Check if the job already exists in the database
+        if not jobs_collection.find_one({'title': job_title, 'location': job_location}):
+            # Insert the job into the database
+            jobs_collection.insert_one({'title': job_title, 'location': job_location})
+            print(f'{job_title} in {job_location} added to the database')
+        else:
+            print(f'{job_title} in {job_location} already exists in the database')
     else:
-        print(f'{job_title} already exists in the database')
+        print("No Matching Job Found")
+
+DatabaseTest()
+
+cards = soup.find_all('div', class_='job-card')
+print(response.main)
+for card in cards:
+    print(card.find("h2", {"class": "job-title"}))
